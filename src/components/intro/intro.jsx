1bc51f81d502dc85140a3188b3fc4bce00800d08
Clem Omotosho <78275155+clementm8@users.noreply.github.com>
2024-11-08 23:35:07 -0700

    intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import './intro.css'
-import bg from '../../assets/clem.png'
-import btnImg from '../../assets/suitcase.svg'
-import { Link } from 'react-scroll'
+import React from 'react';
+import './intro.css';
+import bg from '../../assets/clem.png';
+import btnImg from '../../assets/suitcase.svg';
+import { Link } from 'react-scroll';
+import HireMe from './hire-me';
 
 const Intro = () => {
     return (
@@ -11,7 +12,7 @@ const Intro = () => {
         <span className="hello">Hello,</span>
         <span className="introText"> I&apos;m <span className="introName">Clem </span><br /> Technical Web Specialist</span>
         <p className='introPara'>Tech doesn&apos;t have to be a headache—let me handle it so you can focus on matters most.</p>
-        <Link activeClass='active' to='footer' spy={true} smooth={true} offset={-250} duration={500}><button className="btn"><img src={btnImg} alt="suitcase svg" className='btnImg' style="width: auto; height: auto;"/>Hire Me</button></Link>
+        <Link activeClass='active' to='footer' spy={true} smooth={true} offset={-250} duration={500}><button className="btn"><img src={btnImg} alt="suitcase svg" className='btnImg' style={{ width: 'auto', height: 'auto' }}/>Hire Me</button></Link>
         <div className='tagLine'><i>Success through code</i></div>
         </div>
         <img src={bg} alt="headshot" className="bg" />
